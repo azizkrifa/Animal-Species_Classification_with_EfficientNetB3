40d3b1a220dbd4785c7951302b4ebda54f8aca58
Krifa Mohamed Aziz <azizkrifa33@gmail.com>
2025-05-28 00:11:03 +0100

    Update train.py

--- src/train.py
+++ src/train.py
@@ -32,17 +32,19 @@ history = model.fit(
     callbacks=[early_stop]
 )
 
+model.save('/output/model.h5')
+
 plt.figure(figsize=(12, 4))
-    plt.subplot(1,2,1)
-    plt.plot(history.history['accuracy'], label='Train Acc')
-    plt.plot(history.history['val_accuracy'], label='Val Acc')
-    plt.legend()
-    plt.title('Accuracy')
+plt.subplot(1,2,1)
+plt.plot(history.history['accuracy'], label='Train Acc')
+plt.plot(history.history['val_accuracy'], label='Val Acc')
+plt.legend()
+plt.title('Accuracy')
 
-    plt.subplot(1,2,2)
-    plt.plot(history.history['loss'], label='Train Loss')
-    plt.plot(history.history['val_loss'], label='Val Loss')
-    plt.legend()
-    plt.title('Loss')
+plt.subplot(1,2,2)
+plt.plot(history.history['loss'], label='Train Loss')
+plt.plot(history.history['val_loss'], label='Val Loss')
+plt.legend()
+plt.title('Loss')
 
-    plt.savefig('/output/history.png'))
+plt.savefig('/output/history.png'))
