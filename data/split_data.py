250a133fa2b05e7444e1297790a112c1ee4e5f7a
Krifa Mohamed Aziz <azizkrifa33@gmail.com>
2025-05-28 00:21:58 +0100

    Create split_data.py

---
+++ data/split_data.py
@@ -0,0 +1,12 @@
+!pip install split-folders
+import splitfolders
+
+def split_data():
+  path=load_data()
+  splitfolders.ratio(
+      input=path,
+      output=path+"/split_animals10",
+      seed=1337,
+      ratio=(.7, .2, .1)  # 70% train, 20% val, 10% test
+  )
+  return path
