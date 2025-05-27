e1d1802868e820dbf99835905b8262cab534788f
Krifa Mohamed Aziz <azizkrifa33@gmail.com>
2025-05-28 00:31:17 +0100

    Update split_data.py

--- data/split_data.py
+++ data/split_data.py
@@ -5,8 +5,8 @@ def split_data():
   path=load_data()
   splitfolders.ratio(
       input=path,
-      output=path+"/split_animals10",
+      output="data/split_animals10",
       seed=1337,
       ratio=(.7, .2, .1)  # 70% train, 20% val, 10% test
   )
-  return path
+
