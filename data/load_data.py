0c100d030a06a0c5faa70cce2bfe0d8952d891ef
Krifa Mohamed Aziz <azizkrifa33@gmail.com>
2025-07-01 15:43:24 +0100

    new structure

--- data/load_data.py
+++
@@ -1,4 +0,0 @@
-import kagglehub  #This script downloads the Animals-10 dataset from Kaggle using the kagglehub library.
-def load_data():
-    path = kagglehub.dataset_download("alessiocorrado99/animals10")
-    return path 
